08ab530f12fdf3e5135c61c65bad8846c848ada5
qai <138949452+qaiou@users.noreply.github.com>
2026-01-20 11:38:35 +0800

    changes in css and texts in jsx

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -7,30 +7,34 @@ export  function DashBoard() {
   return (
     <div className="layout">
       <aside className="sidebar">
+        <h2> </h2>
         <nav>
-          <a href="#">User</a>
-          <a href="#">Userns</a>
-          <a href="#">Reports</a>
-          <a href="#">Settings</a>
+          <a href="#">User Account Management</a>
+          <a href="#">Job Listings</a>
+          <a href="#">Mentorship Pairing</a>
+          <a href="#">Events</a>
+          <a href="#">Logout</a>
         </nav>
       </aside>
 
       <main className="main">
         <header className="topbar">
-          <h2>Alumni bla3 system</h2>
-          <button>Logout</button>
+          <h2 className="system">Alumni Engagement and Networking System</h2>
+          <input type="image" src="/user2.png"></input>
         </header>
 
         <section className="content">
           <div className="card">
             <h2>Post 1</h2>
-            lalalllalalaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+            lalalllalalaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<br/>
+            <img src="/gambo.jpg" alt="first class graduates yay congrats" className="image"></img>
+            <br/>first class graduates yay congrats
           </div>
           <div className="card"><h2>Post 2</h2></div>
           <div className="card"><h2>Post 3</h2></div>
           
         </section>
-      </main>
+      </main> 
     </div>
   );
 }
